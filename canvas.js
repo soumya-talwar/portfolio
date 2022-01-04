8d710a1dbdc9ff2d40362187dbf7ee21a0d3390e
Soumya Talwar <soumya.talwar97@gmail.com>
2022-01-04 19:03:47 +0530

    split canvas & landing script files

--- canvas.js
+++ canvas.js
@@ -4,28 +4,12 @@ var num = 30;
 var length = 5;
 
 function setup() {
-  var canvas = createCanvas(windowWidth, windowHeight);
-  canvas.parent("landing");
+  var canvas = createCanvas(windowWidth, $(document).height());
+  canvas.parent("canvas");
   for (let i = 0; i < num; i++) {
     x[i] = 0;
     y[i] = 0;
   }
-  let phrases = [
-    "PROGRAMMER",
-    "DESIGNER",
-    "WRITER",
-    "ARTIST",
-    "FEMINIST"
-  ];
-  let text = new Text(document.getElementById("text"));
-  let counter = 0;
-  const next = () => {
-    text.setText(phrases[counter]).then(() => {
-      setTimeout(next, 800);
-    });
-    counter = (counter + 1) % phrases.length;
-  }
-  next();
 }
 
 function draw() {
@@ -51,67 +35,4 @@ function drag(i, xin, yin) {
   stroke(0);
   line(0, 0, length, 0);
   pop();
-}
-
-class Text {
-  constructor(element) {
-    this.element = element;
-    this.chars = "!<>-_\\/[]{}—=+*^?#________";
-    this.update = this.update.bind(this);
-  }
-
-  setText(newText) {
-    let oldText = this.element.innerText;
-    let length = Math.max(oldText.length, newText.length);
-    let promise = new Promise((resolve) => this.resolve = resolve);
-    this.queue = [];
-    for (let i = 0; i < length; i++) {
-      let from = oldText[i] || "";
-      let to = newText[i] || "";
-      let start = Math.floor(Math.random() * 40);
-      let end = start + Math.floor(Math.random() * 40);
-      this.queue.push({
-        from,
-        to,
-        start,
-        end
-      });
-    }
-    cancelAnimationFrame(this.frameRequest);
-    this.frame = 0;
-    this.update();
-    return promise;
-  }
-
-  update() {
-    let output = "";
-    let complete = 0;
-    for (let i = 0, n = this.queue.length; i < n; i++) {
-      let {
-        from,
-        to,
-        start,
-        end,
-        char
-      } = this.queue[i];
-      if (this.frame >= end) {
-        complete++;
-        output += to;
-      } else if (this.frame >= start) {
-        if (!char || Math.random() < 0.28) {
-          char = this.chars[Math.floor(Math.random() * this.chars.length)];
-          this.queue[i].char = char;
-        }
-        output += `<span class="dud">${char}</span>`;
-      } else
-        output += from;
-    }
-    this.element.innerHTML = output;
-    if (complete === this.queue.length)
-      this.resolve();
-    else {
-      this.frameRequest = requestAnimationFrame(this.update);
-      this.frame++;
-    }
-  }
 }
